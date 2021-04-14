52ef6170c9f377e43383003b8a7b392937111bb7
Pancho <pancho_preciado@hotmail.com>
2021-04-13 21:56:23 -0700

    primer avance

---
+++ app/src/main/java/preciado/francisco/cuidadosadultosmayores/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package preciado.francisco.cuidadosadultosmayores.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
